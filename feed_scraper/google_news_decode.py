c7f8d78a9bd64f3eb04b9683b5ca85cf05a1a7e2
vanalmsick <44527602+vanalmsick@users.noreply.github.com>
2024-08-31 12:36:15 +0100

    Patch google news url decode bug. #patch

--- feed_scraper/google_news_decode.py
+++ feed_scraper/google_news_decode.py
@@ -3,38 +3,10 @@
 
 import requests
 import functools
-import base64
+from googlenewsdecoder import new_decoderv1
 
-# Solution from https://gist.github.com/huksley/bc3cb046157a99cd9d1517b32f91a99e?permalink_comment_id=5132769
-
-
-def fetch_decoded_batch_execute(id):
-    """Extract redirect url from response"""
-    s = (
-        '[[["Fbv4je","[\\"garturlreq\\",[[\\"en-US\\",\\"US\\",[\\"FINANCE_TOP_INDICES\\",\\"WEB_TEST_1_0_0\\"],'
-        'null,null,1,1,\\"US:en\\",null,180,null,null,null,null,null,0,null,null,[1608992183,723341000]],'
-        '\\"en-US\\",\\"US\\",1,[2,3,4,8],1,0,\\"655000234\\",0,0,null,0],\\"' + id + '\\"]",null,"generic"]]]'
-    )
-
-    headers = {"Content-Type": "application/x-www-form-urlencoded;charset=utf-8", "Referer": "https://news.google.com/"}
-
-    response = requests.post(
-        "https://news.google.com/_/DotsSplashUi/data/batchexecute?rpcids=Fbv4je", headers=headers, data={"f.req": s}
-    )
-
-    if response.status_code != 200:
-        raise Exception("Failed to fetch data from Google.")
-
-    text = response.text
-    header = '[\\"garturlres\\",\\"'
-    footer = '\\",'
-    if header not in text:
-        raise Exception(f"Header not found in response: {text}")
-    start = text.split(header, 1)[1]
-    if footer not in start:
-        raise Exception("Footer not found in response.")
-    url = start.split(footer, 1)[0]
-    return url
+# Old Solution from https://gist.github.com/huksley/bc3cb046157a99cd9d1517b32f91a99e?permalink_comment_id=5132769
+# New Solution package googlenewsdecoder
 
 
 @functools.lru_cache(2048)
@@ -43,35 +15,16 @@ def decode_google_news_url(source_url):
     url = requests.utils.urlparse(source_url)
     path = url.path.split("/")
     if url.hostname == "news.google.com" and len(path) > 1 and path[-2] == "articles":
-        base64_str = path[-1]
-        decoded_bytes = base64.urlsafe_b64decode(base64_str + "==")
-        decoded_str = decoded_bytes.decode("latin1")
-
-        prefix = b"\x08\x13\x22".decode("latin1")
-        if decoded_str.startswith(prefix):
-            decoded_str = decoded_str[len(prefix) :]
-
-        suffix = b"\xd2\x01\x00".decode("latin1")
-        if decoded_str.endswith(suffix):
-            decoded_str = decoded_str[: -len(suffix)]
-
-        bytes_array = bytearray(decoded_str, "latin1")
-        length = bytes_array[0]
-        if length >= 0x80:
-            decoded_str = decoded_str[2 : length + 1]
+        decoded_url = new_decoderv1(source_url)
+        if decoded_url.get("status"):
+            return decoded_url["decoded_url"]
         else:
-            decoded_str = decoded_str[1 : length + 1]
-
-        if decoded_str.startswith("AU_yqL"):
-            return fetch_decoded_batch_execute(base64_str)
-
-        return decoded_str
-    else:
-        return source_url
+            print("GNews Decode Error:", decoded_url["message"])
+    return source_url
 
 
 # Example usage
 if __name__ == "__main__":
-    source_url = "https://news.google.com/rss/articles/CBMigAFBVV95cUxNOFBjdjV6eTRLTkxoUU5NZnR3SWk1bS1WRkNPdGJXajBkQjRULUtVLTY0SUN2b0ZxZkZPVlBRNUpENGpGdkx5UGdtMnE5ZUZEQ1c2dVZBVVFzU1VGZjZoalFzcV9Kdkk4a0JkaldsQVdEcHRaM2J4LXdqcGwtTVJmbg?oc=5"
+    source_url = "https://news.google.com/rss/articles/CBMisgFBVV95cUxNWHRheHZncTJIR0FjT2tGTXZ5VHVrc2U4RkQyaldCYUJEaU1URzFLTWNTUEsyRjZ4djFMNng2WkNHbmFUUURKTGJLQ0R3S2xlR1hzVHZMTGs1RnZ2b3R5RjhNbmU3NjVqZXl6NGVZMmZrbW9XM3ZieVVlMlQ5dWlVdzBsSmVQTG5HSk44Z1A0dUNBamZKaTVzMTZUMkJWbjlqbEFfSlhZN3BKLWZVZVdjSlV3?oc=5"
     decoded_url = decode_google_news_url(source_url)
     print(decoded_url)
