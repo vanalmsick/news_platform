12613a8b4224fa8d4d7d1ffff04de4caa0a1afb1
vanalmsick <44527602+vanalmsick@users.noreply.github.com>
2025-04-28 19:46:39 +0100

    Fix not actively traded stocks showing up in Top 5. #patch

--- markets/scrape.py
+++ markets/scrape.py
@@ -175,6 +175,7 @@ def active_gainers_loosers():
         top5["name"] = top5["longName"].fillna(top5["shortName"])
         top5["priceAge"] = (datetime.datetime.now() - pd.to_datetime(top5["regularMarketTime"], unit="s")).dt.total_seconds()
         top5 = top5[top5["priceAge"] < 60 * 60 * 6]  # exclude quotes where the market closed more than 6 hours ago
+        top5 = top5[~((top5["bid"] == 0.0) & (top5["ask"] == 0.0))]  # remove stocks without active trading - i.e. inflated prices
         top5.drop_duplicates(subset=["longName"], inplace=True, keep="first")
         top5 = top5.iloc[: min(len(top5), 5)]
         top5.reset_index(drop=True, inplace=True)
